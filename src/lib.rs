a8283f96555fb3207d06b9f2712f42e6dcb7c220
Krow10 <23462475+Krow10@users.noreply.github.com>
2023-07-14 13:03:40 -0400

    Catch wrong `first_date` formatting for block ranges query

--- src/lib.rs
+++ src/lib.rs
@@ -62,10 +62,15 @@ pub fn pinax_service_v1_blocktime_blockrangebydate(v: Vec<u8>) -> Result<Vec<u8>
     let second_key = match req.second_date {
         Some(second_date) => get_key_by_prefix(format!("block.timestamp:{}", second_date))?,
         None => {
+            let datetime_utc = match NaiveDate::parse_from_str(&req.first_date, "%Y-%m-%d") {
+                // Try to parse naive date
+                Ok(naive_date) => Ok(DateTime::<Utc>::from_utc(naive_date.and_hms_opt(0, 0, 0).unwrap(), Utc)),
+                // Else assume we got a full timestamp
+                Err(_) => req.first_date.parse::<DateTime<Utc>>().map_err(
+                    |e| format!("{:?} -- Wrong `first_date` format, expecting date or timestamp: {:?}", e, req.first_date)
+                )
+            }?;
             // Get next day after `first_date`
-            let naive_date = NaiveDate::parse_from_str(&req.first_date, "%Y-%m-%d").unwrap();
-            let naive_datetime: NaiveDateTime = naive_date.and_hms_opt(0, 0, 0).unwrap();
-            let datetime_utc = DateTime::<Utc>::from_utc(naive_datetime, Utc);
             let next_day_dt = datetime_utc + Duration::days(1);
 
             get_key_by_prefix(format!("block.timestamp:{}", next_day_dt.format("%Y-%m-%d").to_string()))?
