cfeec3fa4132eb22435c498fa6d7daefe86a52b1
Krow10 <23462475+Krow10@users.noreply.github.com>
2023-07-15 13:35:11 -0400

    Update tests script and workflow

--- tests.sh
+++ tests.sh
@@ -13,7 +13,7 @@ fi
 
 ./serve.sh > /dev/null 2>&1 &
 SERVER_PID=$!
-sleep 2s
+sleep 10s
 
 printf "\nTesting \`BlockIdByTime\` queries...\n"
 # Correct
@@ -21,12 +21,14 @@ if grpcurl -plaintext -proto ./proto/service.proto -d '{"timestamp": "2015-07-30
 	echo "{\"timestamp\": \"2015-07-30T15:26:28Z\"} -- OK"
 else
 	echo "{\"timestamp\": \"2015-07-30T15:26:28Z\"} -- FAILED"
+	exit 1
 fi
 
 if grpcurl -plaintext -proto ./proto/service.proto -d '{"timestamp": "2015-07-30"}' localhost:7878 pinax.service.v1.BlockTime.BlockIdByTime | jq -e '.id == "88e96d4537bea4d9c05d12549907b32561d3bf31f45aae734cdc119f13406cb6" and .number == "1"' > /dev/null; then
 	echo "{\"timestamp\": \"2015-07-30\"} -- OK"
 else
 	echo "{\"timestamp\": \"2015-07-30\"} -- FAILED"
+	exit 1
 fi
 
 # Incorrect
@@ -34,12 +36,14 @@ if ! grpcurl -plaintext -proto ./proto/service.proto -d '{"timestamp": "2010-07-
 	echo "{\"timestamp\": \"2010-07-30\"} -- ERROR OK (No data for timestamp)"
 else
 	echo "{\"timestamp\": \"2010-07-30\"} -- ERROR FAILED (No data for timestamp)"
+	exit 1
 fi
 
 if ! grpcurl -plaintext -proto ./proto/service.proto -d '{"timestamp": "2015-07-30 15:26:28"}' localhost:7878 pinax.service.v1.BlockTime.BlockIdByTime 2>/dev/null; then
 	echo "{\"timestamp\": \"2015-07-30 15:26:28\"} -- ERROR OK (Invalid timestamp format)"
 else
 	echo "{\"timestamp\": \"2015-07-30 15:26:28\"} -- ERROR FAILED (Invalid timestamp format)"
+	exit 1
 fi
 
 printf "\nTesting \`BlockTimeById\` queries...\n"
@@ -48,6 +52,7 @@ if grpcurl -plaintext -proto ./proto/service.proto -d '{"number": "1"}' localhos
 	echo "{\"number\": \"1\"} -- OK"
 else
 	echo "{\"number\": \"1\"} -- FAILED"
+	exit 1
 fi
 
 # Incorrect
@@ -55,6 +60,7 @@ if ! grpcurl -plaintext -proto ./proto/service.proto -d '{"number": "-1"}' local
 	echo "{\"number\": \"-1\"} -- ERROR OK (Invalid number)"
 else
 	echo "{\"number\": \"-1\"} -- ERROR FAILED (Invalid number)"
+	exit 1
 fi
 
 printf "\nTesting \`BlockRangeByDate\` queries...\n"
@@ -63,18 +69,21 @@ if grpcurl -plaintext -proto ./proto/service.proto -d '{"first_date": "2015-07-3
 	echo "{\"first_date\": \"2015-07-30T15:26:28Z\"} -- OK"
 else
 	echo "{\"first_date\": \"2015-07-30T15:26:28Z\"} -- FAILED"
+	exit 1
 fi
 
 if grpcurl -plaintext -proto ./proto/service.proto -d '{"first_date": "2015-07-30"}' localhost:7878 pinax.service.v1.BlockTime.BlockRangeByDate | jq -e '.range[0].id == "88e96d4537bea4d9c05d12549907b32561d3bf31f45aae734cdc119f13406cb6" and .range[0].number == "1" and .range[1].id == "ab79f822909750f88dfb9dd0350c1ebe98d5495e9c969cdeb6e0ac993b80175b" and .range[1].number == "6912"' > /dev/null; then
 	echo "{\"first_date\": \"2015-07-30\"} -- OK"
 else
 	echo "{\"first_date\": \"2015-07-30\"} -- FAILED"
+	exit 1
 fi
 
 if grpcurl -plaintext -proto ./proto/service.proto -d '{"first_date": "2015-07-30", "second_date": "2015-08-30"}' localhost:7878 pinax.service.v1.BlockTime.BlockRangeByDate | jq -e '.range[0].id == "88e96d4537bea4d9c05d12549907b32561d3bf31f45aae734cdc119f13406cb6" and .range[0].number == "1" and .range[1].id == "c76c70bf56e8b040e8fe4a0ecddba98f30ddbaf0b244749b8b4ce6ad3471eb66" and .range[1].number == "162118"' > /dev/null; then
 	echo "{\"first_date\": \"2015-07-30\", \"second_date\": \"2015-08-30\"} -- OK"
 else
 	echo "{\"first_date\": \"2015-07-30\", \"second_date\": \"2015-08-30\"} -- FAILED"
+	exit 1
 fi
 
 # Incorrect
@@ -82,12 +91,14 @@ if ! grpcurl -plaintext -proto ./proto/service.proto -d '{"first_date": "2010-07
 	echo "{\"first_date\": \"2010-07-30\"} -- ERROR OK (No data for timestamp)"
 else
 	echo "{\"first_date\": \"2010-07-30\"} -- ERROR FAILED (No data for timestamp)"
+	exit 1
 fi
 
 if ! grpcurl -plaintext -proto ./proto/service.proto -d '{"first_date": "2015-07-30 15:26:28"}' localhost:7878 pinax.service.v1.BlockTime.BlockRangeByDate 2>/dev/null; then
 	echo "{\"first_date\": \"2015-07-30 15:26:28\"} -- ERROR OK (Invalid timestamp format)"
 else
 	echo "{\"first_date\": \"2015-07-30 15:26:28\"} -- ERROR FAILED (Invalid timestamp format)"
+	exit 1
 fi
 
 pkill -P ${SERVER_PID}
